dffce451e3ac98fb2c3669423667b7728e7323b9
JOE MARC YOUNES <marcyounes22@gmail.com>
2025-01-14 20:43:05 -0500

    Add project files.

---
+++ QuestionFourteen/Helper/GenerateNumbers.cs
@@ -0,0 +1,16 @@
+﻿namespace QuestionFourteen.Helper
+{
+    public class GenerateNumbers
+    {
+        public static int GenerateRandomNumber()
+        {
+            Func<int> randomNumberFunc = () =>
+            {
+                Random rand = new Random();
+                return rand.Next(1, 10001);
+            };
+
+            return randomNumberFunc();
+        }
+    }
+}
---
+++ QuestionFourteen/Program.cs
@@ -0,0 +1,30 @@
+﻿using QuestionFourteen.Helper;
+
+namespace QuestionFourteen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => GenerateNumbers.GenerateRandomNumber())
+                .ContinueWith(task =>
+                {
+                    return Format(task.Result);
+                })
+                .ContinueWith(task =>
+                {
+                    Console.WriteLine(task.Result);
+                });
+
+            Console.ReadLine();
+        }
+
+
+        public static string Format(int number)
+        {
+            return $"The Generated Number is: {number}";
+        }
+    }
+}
+
+
